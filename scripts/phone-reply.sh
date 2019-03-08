5ee1c538bfdb9d0be8589811ee9ff30b831d30cb
Christopher Witt <github@ch-witt.de>
2019-03-08 16:00:33 +0100

    updated phone-reply.sh so that only replyable messages can be replied to

--- scripts/phone-reply.sh
+++ scripts/phone-reply.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-#put your device id here. You can get it from 'kdeconnect-cli -l'
-device=sjfdgjsdgjassd
-#getting the number of the first notification
+device=42c3c7a3f203ade1
 notification1=`dbus-send --session --print-reply --dest="org.kde.kdeconnect" /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.notifications.activeNotifications|tr '\n' ' ' | awk '{print $12}'| sed s/\"//g`
-#echo "$notification1"
-#this actually opens up the same reply window the plasmoid on kde does
-qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device/notifications/$notification1 org.kde.kdeconnect.device.notifications.notification.reply
+replyable=`qdbus org.kde.kdeconnect /modules/kdeconnect/devices/42c3c7a3f203ade1/notifications/$notification1 org.kde.kdeconnect.device.notifications.notification.replyId`
+if [ "$replyable" ]; then 
+	qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device/notifications/$notification1 org.kde.kdeconnect.device.notifications.notification.reply
+fi
