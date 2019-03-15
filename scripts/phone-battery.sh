427849698b10d1aec05aafe263cda7fcb98b2147
Christopher Witt <github@ch-witt.de>
2019-03-15 19:27:58 +0100

    added a symbol to indicate the phone being charged

--- scripts/phone-battery.sh
+++ scripts/phone-battery.sh
@@ -2,6 +2,12 @@
 #put your device id here. You can get it from 'kdeconnect-cli -l'
 device=sjfdgjsdgjassd
 #sending a dbus command that returns the charge of the battery and a bunch of junk that is cleared before usage in polybar
-charge=`dbus-send --session --print-reply --dest="org.kde.kdeconnect" /modules/kdeconnect/devices/42c3c7a3f203ade1 org.kde.kdeconnect.device.battery.charge|tr '\n' ' ' | awk '{print $10}'`
-#echo the value and a percentage sign, which is what polybar displays
-echo "$charge%"
+charge=`dbus-send --session --print-reply --dest="org.kde.kdeconnect" /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.battery.charge|tr '\n' ' ' | awk '{print $10}'`
+charging=`qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$device org.kde.kdeconnect.device.battery.isCharging`
+#display either the percentage or the percentage plus a symbol to indicate charging
+if [ $charging == "true" ]; then
+	echo "$charge%"
+else
+	echo "$charge%"
+fi
+
